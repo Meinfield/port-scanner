c52f2e91d62fc5f99387a90726ecae9a628863fc
Phillip Anderson <phill.eanderson@gmail.com>
2017-02-23 05:51:27 -0700

    Able to write to HTML file

--- port_scanner.py
+++ port_scanner.py
@@ -15,11 +15,13 @@ def signal_handler(signal, frame):
         print('[1] Closing Program Due to User Interrupt!')
         sys.exit(0)
 
+# globalstart_time = datetime.now()
+# globalend_time = datetime.now()
 
 def checkHostLive(hostaddress):
 	conf.verb = 0 #disable any output from scapy itself
 	try:
-		ping = sr1(IP(dst = hostaddress)/ICMP(), timeout=1) #craft icmp packet
+		ping = sr1(IP(dst = hostaddress)/ICMP(), timeout=3) #craft icmp packet
 		print("[*] Target is up, Beginning Scan")
 	except Exception:
 		print("\n[!] Unable to find host!")
@@ -29,38 +31,40 @@ def checkHostLive(hostaddress):
 def TCPscanThePort(hostaddress, portnumber):
 	srcport = RandShort() #pick a random number for the source port
 	conf.verb = 0 #disable any output from scapy itself
-	SYNACKpkt = sr1(IP(dst = hostaddress)/TCP(sport = srcport, dport = portnumber, flags = 'S'), timeout=10)
+	SYNACKpkt = sr1(IP(dst = hostaddress)/TCP(sport=srcport,dport=portnumber,flags='S'),timeout=10)
 	#set the destination, the source port, and destination port. Flag S means SYN packet
-	print("hey")
-	print(SYNACKpkt.getlayer(TCP).flags)
-	try:
-		if SYNACKpkt is None:
-			return False
+	if str(type(SYNACKpkt)) == "<type 'NoneType'>": #attempts to find filtered ports
+		return 2
+	elif SYNACKpkt.haslayer(TCP):
 		if SYNACKpkt.getlayer(TCP).flags == 0x12:
-			return True
-		else:
-			return False
-	except:
-		return False
-	RSTpkt = IP(dst = hostaddress)/TCP(sport = srcport, dport = portnumber, flags = "R")
-	#send the reset packet to close the connection
-	send(RSTpkt)
+			RSTpkt = sr(IP(dst = hostaddress)/TCP(sport=srcport,dport=portnumber,flags="R"),timeout=10)
+			#send the reset packet to close the connection
+			return 1
+		elif SYNACKpkt.getlayer(TCP).flags == 0x14:
+			return 0
+	elif SYNACKpkt.haslayer(ICMP): #again attempts to find filtered ports
+		if(int(SYNACKpkt.getlayer(ICMP).type) == 3 and int(SYNACKpkt.getlayer(ICMP).code) in [1,2,3,9,10,13]):
+			return 2
+	else:
+		return 0
 
 def UDPscanThePort(hostaddress, portnumber):
-	dst_timeout=5
-	udp_scan_resp = sr1(IP(dst=hostaddress)/UDP(dport=portnumber),timeout=dst_timeout)
-	try:
-		if udp_scan_resp is None:
-			return False
-		if UDP in udp_scan_resp:
-			return True
-		else:
-			return False
-	except:
-		return False
+	udp_scan_resp = sr1(IP(dst=hostaddress)/UDP(dport=portnumber),timeout=10)
+	
+	if str(type(udp_scan_resp)) == "<type 'NoneType'>": #attempts to find filtered ports
+		return 2
+	if udp_scan_resp.haslayer(UDP):
+		return 1
+	elif udp_scan_resp.haslayer(ICMP): #again attempts to find filtered ports
+		if(int(udp_scan_resp.getlayer(ICMP).type) == 3 and int(udp_scan_resp.getlayer(ICMP.code) == 3)):
+			return 0
+		if(int(udp_scan_resp.getlayer(ICMP).type) == 3 and int(udp_scan_resp.getlayer(ICMP).code) in [1,2,9,10,13]):
+			return 2
+	else:
+		return 0
 
 def ICMPscan(hostaddress, portnumber):
-	ping = sr1(IP(dst = hostaddress)/ICMP(dport=portnumber), timeout=1) #craft icmp packet
+	ping = sr1(IP(dst = hostaddress)/ICMP(dport=portnumber), timeout=3) #craft icmp packet
 	try:
 		if ping is None:
 			return False
@@ -72,66 +76,11 @@ def ICMPscan(hostaddress, portnumber):
 	sys.exit(1)
 
 def traceroute(destinationaddress):
-	hostname = "google.com"
-	for i in range(1, 28):
-		pkt = IP(dst=hostname, ttl=i) / UDP(dport=33434)# Send the packet and get a reply
-		reply = sr1(pkt, verbose=0)
-		if reply is None:
-			break
-		elif reply.type == 3:
-			print("Done!", reply.src)
-			break
-		else:
-			print("%d hops away: " % i , reply.src)
-	# try:
-	# 	for hopnumber in range (1,30):
-	# 		#send and receive packet using a random, probably closed port
-	# 		replypkt = sr1(IP(dst=destinationaddress, ttl=hopnumber)/UDP(dport=22222), timeout=2, verbose=0)
-	# 		if replypkt is None:
-	# 			break
-	# 		elif replypkt.type == 3:
-	# 			print("[*] Reached Destination ", replypkt.src)
-	# 		else:
-	# 			print(" %d: " %hopnumber, replypkt.src)
-	# 	sys.exit(1)
-	# except:
-	# 	print("\n[!] Unknown problem detected!")
-	# 	print("[!] Exiting program...")
-	# 	sys.exit(1)
-
-	# flag = True
-	# ttl=1
-	# hops = []
-	# while flag and ttl!=25:
-	# 	ans, unans = sr(IP(dst=destinationaddress,ttl=ttl)/UDP(dport=22222), timeout=3)
-	# 	print(ans.res[0][1].type)
-	# 	if ans.res[0][1].type == 0x12: # checking for  ICMP echo-reply
-	# 		flag = False
-	# 		print("help")
-	# 		print(hops)
-	# 	else:
-	# 		hops.append(ans.res[0][1].src) # storing the src ip from ICMP error message
-	# 		print(hops)
-	# 		ttl +=1
-	# i = 1
-	# for hop in hops:
-	# 	print(i, " ", hop)
-	# 	i+=1
-	# sys.exit(1)
-
-	# ans,unans=sr(IP(dst=destinationaddress, ttl=(1,25))/TCP(flags=0x2))
-	# try:
-	# 	for snd,rcv in ans:
-	# 		print(" " + snd.ttl + " Address: " + rcv.src)
-	# except:
-	# 	for snd,rcv in ans:
-	# 		print(" ", snd.ttl, " Address: ", rcv.src)
-	# 	print("\n[!] Unknown problem detected!")
-	# 	print("[!] Exiting program...")
-	# 	sys.exit(1)
-	# sys.exit(1)
+	ans,unans=sr(IP(dst=destinationaddress,ttl=(1,10))/TCP(dport=53,flags="S"))
+	ans.summary( lambda(s,r) : r.sprintf("%IP.src%\t{ICMP:%ICMP.type%}\t{TCP:%TCP.flags%}"))
 
 def initiatePortScan(ip):
+	resultslist = [] #location to place everything into for writing to a file
 	#range of ports
 	try:
 		portrange = args.port
@@ -154,12 +103,16 @@ def initiatePortScan(ip):
 						status = UDPscanThePort(ip, int(port))
 					else:
 						status = TCPscanThePort(ip, int(port))
-
-					if status == True:
+					if status == 1:
+						resultslist.append("Port " + str(port) + ": Open")
 						print("Port " + str(port) + ": Open")
+					elif status == 2:
+						resultslist.append("Port " + str(port) + ": Filtered")
+						print("Port " + str(port) + ": Filtered")
 					else:
+						resultslist.append("Port " + str(port) + ": Closed")
 						print("Port " + str(port) + ": Closed")
-					return
+				return resultslist
 			except:
 				print("\n[!] Unknown problem detected!")
 				print("[!] Exiting program...")
@@ -168,34 +121,69 @@ def initiatePortScan(ip):
 			checkHostLive(ip)
 			print("[*] Initiated scanning at " + strftime("%H:%M:%S") + "\n")
 			if str(args.protocol) == "UDP":
+
 				status = UDPscanThePort(ip, int(args.port))
+				print("why")
 			else:
 				status = TCPscanThePort(ip, int(args.port))
-
-			if status == True:
+			
+			if status == 1:
+				resultslist.append("Port " + str(args.port) + ": Open")
 				print("Port " + str(args.port) + ": Open")
+			elif status == 2:
+				resultslist.append("Port " + str(args.port) + ": Filtered")
+				print("Port " + str(args.port) + ": Filtered")
 			else:
+				resultslist.append("Port " + str(args.port) + ": Closed")
 				print("Port " + str(args.port) + ": Closed")
-			return
+			return resultslist
 	except:
 		#no port specified
 		for port in range(1,101):
 			if str(args.protocol) == "UDP":
 				status = UDPscanThePort(ip, int(port))
 			else:
-				print('sdhk')
 				status = TCPscanThePort(ip, int(port))
-			if status == True:
+			if status == 1:
+				resultslist.append("Port " + str(port) + ": Open")
 				print("Port " + str(port) + ": Open")
+			elif status == 2:
+				resultslist.append("Port " + str(port) + ": Filtered")
+				print("Port " + str(port) + ": Filtered")
 			else:
+				resultslist.append("Port " + str(port) + ": Closed")
 				print("Port " + str(port) + ": Closed")
+		return resultslist
+
+def writeToFile(resultslist):
+	print("[*] Writing to File " + str(args.write))
+	filename = args.write + '.html'
+	f = open(filename,'w')
+
+	wrapper = """<html>
+	<title>Port Scan Results</title>
+		<body>
+			<h2>Phillip's Fantastic Port Scanner</h2>
+			<h4>Results:</h4>
+			<ul>"""
+	for i in resultslist:
+			wrapper += '<li>%s</li>' % i
+
+	wrapper += """
+			</ul>
+		</body>
+	</html>"""
+
+	f.write(wrapper)
+	f.close()
+
 
 parser = argparse.ArgumentParser(description='This is a simple port scanner')
 parser.add_argument('-i', '--ipaddress', action="store", help='Type in IP address of host machine; Type in multiple hosts by adding a "-<last host\'final two digits>"')
 parser.add_argument('-p', '--port', action="store", help='Type in port(s) you wish to scan (use a dash to indicate the range), default is ports 1 -100')
 parser.add_argument('-t', '--traceroute', action="store", help='Type in IP address you wish to get a route to')
 parser.add_argument('-u', '--protocol', action="store", help='Type in UDP or TCP after, default is TCP')
-
+parser.add_argument('-w', '--write', action="store", help='Type in the filename you would like to save the results as')
 args = parser.parse_args()
 
 start_time = datetime.now()
@@ -214,6 +202,8 @@ if args.traceroute is None and args.ipaddress is None:
 #check for traceroute
 if args.traceroute is not None:
 	traceroute(args.traceroute)
+
+resultslist = [] #this will eventually contain all results
 
 #range of ip addresses
 iprange = args.ipaddress
@@ -237,11 +227,18 @@ try:
 		iprange = range(int(ipseries[3]), int(iplist[1])+1)
 		for ip in iprange:
 			print("[->] IP Address: " + str(actualip) + str(ip))
-			initiatePortScan(str(actualip) + str(ip))
+			resultslist.append(str(actualip) + str(ip))
+			listofports = initiatePortScan(str(actualip) + str(ip))
+			resultslist.append(listofports)
 	else:
-		initiatePortScan(args.ipaddress)
+		resultslist.append(str(args.ipaddress))
+		listofports = initiatePortScan(args.ipaddress)
+		resultslist.append(listofports)
 except:
 	sys.exit(1)
+
+if args.write is not None:
+	writeToFile(resultslist)
 
 end_time = datetime.now()
 elapsed_time = end_time - start_time
